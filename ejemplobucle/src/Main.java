f3115baf60e19ac2109c744444aaf719cbb111d9
christian <chrisferruae@gmail.com>
2022-08-09 17:09:00 -0500

    Segundo ejercicio a la rama main

---
+++ ejemplobucle/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int n=0;
+        while(true){
+            System.out.println("Hello world!" + n);
+        }
+        do {
+            System.out.println("Hello world!" + n);
+        }while (n);
+
+    }
+}
