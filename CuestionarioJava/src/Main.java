a062c8cf215e02623ea4e24e330ae9ffcae8c079
christian <chrisferruae@gmail.com>
2022-08-09 17:34:20 -0500

    corrigiendo el codigo

--- CuestionarioJava/src/Main.java
+++ CuestionarioJava/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
       int lista[] = {10,9,-8,2,3,100,3};
       int num1 = lista[0];
       int num2 = lista[0];
-      int num3 = lita[1];
+      int num3 = lista[1]; //Corregi el error que tenia para que se note el cambio
       String periodo;
       double num233 = 5.2;
       
